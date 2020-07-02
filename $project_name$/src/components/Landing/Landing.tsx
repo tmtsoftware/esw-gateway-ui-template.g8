768ec666440983958a46d09111b4625e10350d2b
ManishGowardipe <gowardipemanish95@gmail.com>
2020-07-02 18:34:26 +0530

    #ESW-335 : Manish :  use esw-ts library from npm

--- $project_name$/src/components/Landing/Landing.tsx
+++ $project_name$/src/components/Landing/Landing.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import logo from "../../assets/images/tmt_logo.png";
+import React from 'react'
+import logo from '../../assets/images/tmt_logo.png'
 import './Landing.css'
 
 const Landing = () => {
   return (
     <div>
-      <img className="logo" src={logo} />
+      <img className='logo' alt='logo' src={logo} />
       <h3>Update src/App.tsx and save to reload.</h3>
+      
       <h3>
-        Refer esw-ts library usage from <a
-          className="link"
-          href="https://github.com/tmtsoftware/esw-ts"
-        >
+        Refer esw-ts library usage from{' '}
+        <a className='link' href='https://github.com/tmtsoftware/esw-ts'>
           here.
         </a>
       </h3>
     </div>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
--- $project_name$/test/components/Landing/Landing.test.tsx
+++ $project_name$/test/components/Landing/Landing.test.tsx
@@ -3,7 +3,7 @@ import { Landing } from '../../../src/components'
 import { render } from '@testing-library/react'
 
 describe('Landing', () => {
-  test('should render Landing page', async () => {
+  test('should render', async () => {
     const { container } = render(<Landing />)
     expect(container).toMatchSnapshot()
   })
