ce1b3193fc2230df144c8c2687338c55884d8d19
ManishGowardipe <gowardipemanish95@gmail.com>
2020-07-20 12:06:46 +0530

    ESW-202: Himanshu | Manish : Use generic client-id for frontend template

--- $project_name$/src/config/AppConfig.ts
+++ $project_name$/src/config/AppConfig.ts
@@ -1,6 +1,6 @@
 // #app-config
 export const AppConfig = {
   realm: 'TMT', // Realm of your client
-  clientId: 'esw-app-client' // Client id which is registered in AAS server
+  clientId: 'tmt-frontend-app' // Client id which is registered in AAS server
 }
 // #app-config
