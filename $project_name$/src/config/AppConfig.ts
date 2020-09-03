a3e4ba2be0156a4b07fe584cb07dfab9f45b82fb
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2020-09-03 16:09:02 +0530

    #ESW-312: Manish | Vinay: Add Application name in AppConfig file
    Co-authored-by: Manish Gowardipe <gowardipemanish95@gmail.com>

--- $project_name$/src/config/AppConfig.ts
+++ $project_name$/src/config/AppConfig.ts
@@ -1,6 +1,7 @@
 // #app-config
 export const AppConfig = {
   realm: 'TMT', // Realm of your client
-  clientId: 'tmt-frontend-app' // Client id which is registered in AAS server
+  clientId: 'tmt-frontend-app', // Client id which is registered in AAS server
+  applicationName: '$project_name$' //Application name is used for capturing metrics of this web application
 }
 // #app-config
