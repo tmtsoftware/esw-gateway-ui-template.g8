861ecc3af762a048534bf1cb44e1d2e59d84c534
Shubham Jaybhaye <shubhamj7997@gmail.com>
2021-06-24 17:37:05 +0530

    #ESW-431: Vinay| Shubham J: update template according to latest deployment stratergy

---
+++ $project_name$/snowpack.config.js
@@ -0,0 +1,18 @@
+import { AppConfig } from './src/config/AppConfig.js'
+export default {
+  buildOptions: {
+    clean: true,
+    sourceMaps: false,
+    out: AppConfig.applicationName
+  },
+  mount: {
+    public: "/",
+    src: "/dist",
+    test: "/dist_test"
+  },
+  plugins: [["@snowpack/plugin-typescript"]],
+  alias: {
+    "io-ts/lib": "io-ts/es6",
+    "fp-ts/lib": "fp-ts/es6"
+  }
+}
--- $project_name$/src/App.tsx
+++ $project_name$/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Landing } from './components'
 import './App.css'
+import { AppConfig } from './config/AppConfig'
+
+const basename = import.meta.env.NODE_ENV === 'production' ? `/${AppConfig.applicationName}` : ''
 
 const App = (): JSX.Element => (
-  <div className='App'>
+  <Router basename={basename}>
+    <div className='App'>
     <Landing />
   </div>
+  </Router>
 )
 
 export default App
