933d6e8fb250e6980d091a4c0ff851c4aec511e2
Manish Gowardipe <gowardipemanish95@gmail.com>
2021-09-14 11:51:28 +0530

    #ESW-416: Manish : Update App.tsx

--- src/main/g8/$project_name$/src/App.tsx
+++ src/main/g8/$project_name$/src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import { LocationService } from '@tmtsoftware/esw-ts'
+import { LocationService, loadGlobalConfig } from '@tmtsoftware/esw-ts'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppConfig } from './config/AppConfig'
 import { LocationServiceProvider } from './contexts/LocationServiceContext'
 import { useQuery } from './hooks/useQuery'
 import { Main} from './components/Main'
 
-const basename = import.meta.env.NODE_ENV === 'production' ? `/${AppConfig.applicationName}` : ''
+const basename =
+  import.meta.env.NODE_ENV === 'production'
+    ? `/${AppConfig.applicationName}`
+    : ''
 
 const App = (): JSX.Element => {
-  const { data: locationService, loading, error } = useQuery(LocationService)
+  const { data: initialised, error } = useQuery(() => loadGlobalConfig().then(() => true))
+  const locationService = LocationService()
 
-  if (loading) return <div>Loading...</div>
-  if (error || !locationService)
-    return <div>Location Service not Available, reason {error?.message}</div>
-
-  return (
+  if (error) return <div> Failed to load global config </div>
+  return initialised ? (
     <LocationServiceProvider locationService={locationService}>
       <Router basename={basename}>
         <Main />
       </Router>
     </LocationServiceProvider>
+  ) : (
+    <div>Loading....</div>
   )
 }
 
